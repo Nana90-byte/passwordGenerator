de5c8f52c8d325dd5178cd90b5c58a1613967832
mariarosadelatex <mariarosadelatex@gmail.com>
2019-10-01 17:21:00 -0600

    changed name files to script and style

---
+++ script.js
@@ -0,0 +1,72 @@
+
+   var upperCase = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+   var lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+   var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+   var special = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","\","|","]
+
+    
+    var arr = []
+    
+    var passTxtbox = document.getElementById("#password");
+    var clipBoard = document.getElementById("#copy");
+    var finalPass = "";
+    
+    
+    var generatePass = function() {
+    alert("Password must contain between 8 to 128 characters");
+    var passwordLength = prompt("How many characters would you like?");
+    
+    if (passwordLength < 8) 
+    return alert("It has to be at least 8 characters!");
+    if (passwordLength > 128)
+    return alert("No more than 128 characters");
+    if (passwordLength < 8 || passwordLength > 128)
+   
+    
+    
+    var upperLength = confirm("Would you like upper case letters?");
+    
+    var lowerLength = confirm("Would you like lower case letters?");
+    
+    var numbersLength = confirm("Would you like numbers?");
+    
+    var specialLength = confirm("Would you like special characters?");
+    console.log(upperLength)
+
+    console.log(arr)
+    if (upperLength) {
+    arr = arr.concat(upperCase)
+    }
+    if (lowerLength) {
+    arr = arr.concat(lowerCase)
+    }
+    if (numbersLength) {
+    arr = arr.concat(numbers)
+    }
+    if (specialLength) {
+    arr = arr.concat(special)
+    }
+    
+    console.log(arr)
+    for(var i = 0; i < passwordLength; i++) {
+        var x = Math.floor(Math.random()*arr.length);
+        finalPass += arr[x];
+} password.innerHTML = finalPass;
+}
+    
+    
+
+    var myfunction = function() {
+        var clipBoard = document.getElementById("password");
+        clipBoard.select();
+        clipBoard.setSelectionRange(0,99999);
+        document.execCommand("copy");
+        alert("Copied the text: " + clipBoard.value);
+    } 
+    
+    
+    
+    
+    
+        
+       
